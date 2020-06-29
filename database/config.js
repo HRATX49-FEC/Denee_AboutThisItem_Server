fb9b16a4362087c378fea0888a27219baf0bf775
Denee Lopez <denee.lopez@gmail.com>
2020-06-29 11:29:41 -0500

    try again

--- database/config.js
+++ database/config.js
@@ -2,5 +2,5 @@ module.exports = {
   host: process.env.RDS_HOSTNAME || 'localhost',
   user: process.env.RDS_USERNAME || 'root',
   password: process.env.RDS_PASSWORD || 'maddy20bo',
-  database: process.env.RDS_DB_NAME || 'purrget'
+  database: 'purrget'
 };
