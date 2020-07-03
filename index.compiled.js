459c90c72d39cabee3888842324f47bf22ea031f
Denee Lopez <denee.lopez@gmail.com>
2020-07-02 20:14:36 -0500

    really the final MVP

--- index.compiled.js
+++ index.compiled.js
@@ -6,36 +6,34 @@ var path = require('path');
 
 var db = require('./database/query.js');
 
-var app = express();
-
 var compression = require('compression');
 
+var app = express();
 var PORT = process.env.PORT || 5100;
-app.use(express.json());
 app.use(compression());
+app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(express["static"](path.join(__dirname, 'client', 'dist')));
+app.use(express["static"](path.join(__dirname, 'client', 'dist'))); // get searched cat
+
 app.get('/about/cat', function (req, res) {
-  // console.log('get', req.query.catName)
   db.getCats(req.query.catName, function (error, results) {
     if (error) {
       console.error('error getting query', error);
-      res.send(error, null);
+      res.sendStatus(404);
     } else {
-      // console.log('query results:', results);
       res.status(200).send(results);
     }
   });
-});
+}); // get searched cat questions and answers
+
 app.get('/about/questions', function (req, res) {
   db.getQuestions(req.query.catName, function (error, results) {
     if (error) {
       console.error('error getting query', error);
-      res.send(error, null);
+      res.sendStatus(404);
     } else {
-      // console.log('query results:', results);
       res.status(200).send(results);
     }
   });
@@ -53,13 +51,10 @@ app.get('/about/questions', function (req, res) {
 // });
 
 app.post('/about/question', function (req, res) {
-  console.log('in /about/question');
   db.addQuestion(req.body.question, function (error, results) {
-    console.log('results', results);
-
     if (error) {
       console.error(error);
-      res.send(error, null);
+      res.sendStatus(404);
     } else {
       res.status(200).send('sent');
     }
@@ -68,7 +63,8 @@ app.post('/about/question', function (req, res) {
 app.post('/about/answer', function (req, res) {
   db.addAnswer(req.body.answer, function (error, results) {
     if (error) {
-      res.send(error, null);
+      console.error(error);
+      res.sendStatus(404);
     } else {
       res.status(200).send('sent');
     }
